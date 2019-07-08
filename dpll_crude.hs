c4ac000a3ec23de6d8ac040d3e323007a9028f98
Gavin Peng <gavinsweden@gmail.com>
2019-07-08 17:08:38 +0100

    0807/2

--- dpll_crude.hs
+++ dpll_crude.hs
@@ -1,5 +1,6 @@
 import Data.List
 
+-- Using the standard SAT input format
 e :: [[Int]]
 e = [[-1,  2,  3],
      [ 1,  3,  4],
@@ -12,15 +13,23 @@ e = [[-1,  2,  3],
 
 dpll :: [[Int]] -> [Int] -> [Int]
 dpll e v
-    | e == []   = v
-    | elem [] e = []
-    | length units /= 0 =
-        dpll (propagate usym e) (v ++ [usym])
-    | dpll (propagate sym e) (v ++ [sym]) /= [] = dpll (propagate sym e) (v ++ [sym])
-    | otherwise = dpll (propagate (-sym) e) (v ++ [(-sym)])
+    -- SAT
+    | e == [] = v   
+    -- UNSAT
+    | elem [] e = []    
+    -- Test unit propagation
+    | length units /= 0 = dpll (propagate usym e) (v ++ [usym])
+    -- Propagate the first symbol
+    | otherwise = dpll (propagate sym e) (v ++ [sym]) >||< dpll (propagate (-sym) e) (v ++ [(-sym)])
         where
+            -- Get the unit clauses
             units = filter (\x -> (length x) == 1) e
+            -- Get the symbol for unit propagation, only invoked if units is non-empty
             usym = head $ head units
+            -- Get the first symbol
             sym = head $ head e
+            -- Propagation helper: first delete clauses then remove opposite polarity
             propagate n e = map (\\ [-n]) $ filter (notElem n) e
+            -- Acts like || 
+            (>||<) x y = if x /= [] then x else y
 
